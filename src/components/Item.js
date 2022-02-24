e7c57d7556a2c6ba421989c06509ab33fe433f01
Michael Dally <mddally@gmail.com>
2022-02-24 12:01:07 -0500

    commit tree 576f67343ff8a7910dd8c9c99644b6c1e1a21fc2

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  const [inCart, toggleInCart] = useState(false);
+  function handleCartToggle(){
+    toggleInCart(() => !inCart);
+  }
   return (
-    <li className="">
+    <li className={inCart ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button 
+        className="add"
+        onClick={handleCartToggle}
+      >
+        {!inCart ? "Add to Cart" : "Remove from Cart"}
+      </button>
     </li>
   );
 }
